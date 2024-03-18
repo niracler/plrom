e48f1acce7468c5827de5ce221e9755b29b7eaa8
niracler <i@niracler.com>
2024-03-18 16:19:38 +0800

    docs: add other case

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ content="$(< README.md jq -Rs -r '@json')"
 
 flag=0
 while read -r line; do
-    if [ "$line" = "---" ]; then
+    if [ "$line" = "<!--" ] || [ "$line" = "-->" ]; then
         if [ "$flag" -eq 0 ]; then
             flag=1
             elif [ "$flag" -eq 1 ]; then
